b8259eb9e5ec31458037fae376aa256ec7aafa33
Danish Ahmed <77854446+Danishahmedkhan11@users.noreply.github.com>
2023-02-10 18:08:09 +0000

    Update code for refactor

--- main.py
+++ main.py
@@ -1,69 +1,79 @@
 #Step 2
 
-import random
-from arts import stages
-from arts import logo
-from word_list import word_list
+import random   #import random from library which is already pre-built
 
+from arts import stages,logo   #import from arts.py
+from word_list import word_list  #import form word_list.py
+
+#generate a random to choose any word from word_list
 chosen_word = random.choice(word_list)
 
-#Testing code
-
-
-#TODO-1: - Create an empty List called display.
+#display the word which you guess
+#make sure empty array in display
 display=[]
-#For each letter in the chosen_word, add a "_" to 'display'.
 
+
+#For each letter in the chosen_word, add a "_" to 'display'.
 for _ in range(len(chosen_word)):
   display+='_'
 
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+#check if the end of game is finsihed or not yet.
+#make sure to assign false as it starts game.
+end_game=False
 
-
-#TODO-2: - Loop through each position in the chosen_word;
-
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-end_reach=False
+#only 7 times left you can play which you have error.
 alive=7
+
+#save letter which you type and guess
 letters=[]
+
+#show logo from arts.py
 print(logo)
 
+#Check if it has reached the game or not yet
+#if it has not finised, then go on running like looping
+#if it has the end of game, then stop it.
 
-while not end_reach:
+while not end_game:
+  
+  #input from user
   guess = input("Guess a letter: ").lower()
-
+  
+  #check if it is matche or not
   for postition in range(len(chosen_word)):
+
+    #make sure you have not written the same letter or character twice or more time
+    #it is used to check if you have written the same letter, it show 'Already guessed' and also 1       point deduced alive.
     if display[postition]==guess:
       print('Already guessed')
       alive-=1
       print(stages[alive])
       break
-      
+
+    #check if it is match or not
+    #if it is true match, then save and assingn value to display with postition
     if chosen_word[postition] == guess:
       display[postition]=guess
       letters+=guess
 
+  #check if the letter you type is not contain in chosen_word.
+  # if it is true that your letter is not contain in chosen_word. it applies 1 point to deduce alive
   if guess not in chosen_word:
     alive-=1
     print(stages[alive])
 
+  #show the the group of letters you guess
   print('\n')
   print((' ').join(display))
   print('\n')
 
+  #if alive is equal to 0, it means you fail. no more game as hang man is already gone.
   if alive==0:
     print('You failed!')
     print('The word is acutally '+chosen_word)
-    end_reach=True
-  
+    end_game=True
+
+  #if the word is perfect from your guess, it means you win the game and the end of game.
   if('_' not in display):
-    print('\nYou Sucess!!')
-    
-    end_reach=True
-
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-
-
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+    print('\nYou Won!!')
+    end_game=True
